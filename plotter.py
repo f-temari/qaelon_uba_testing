0da99408ac112c33dd2d5f37fb83261ec00cd17a
fares-temari <fares.temari@outlook.com>
2022-11-23 16:23:56 +0100

    same commit, diffrent day

--- plotter.py
+++ plotter.py
@@ -1,64 +1,100 @@
 import serial  # import Serial Library
-import csv
+import csv  # import csv library
 import numpy
 from datetime import datetime
-import matplotlib.pyplot as plt  # import matplotlib library
+import matplotlib.pyplot  # import matplotlib library
 from drawnow import *
 
 
 # Variable declations
-Raw = []
-Pressure = []
-cnt = 0
+temperature = []
+elvh_pressure = []
+lpdr_pressure = []
 
 # datetime object containing current date and time
 now = datetime.now()
-# dd/mm/YY H:M:S
 dt_string = now.strftime("%d%m%Y_%H%M%S")
 
-arduinoData = serial.Serial("com3", 9600)  # Creating our serial object
+# Creating our serial object
+arduinoData = serial.Serial("com3", 9600)
 
-plt.ion()  # Tell matplotlib you want interactive mode to plot live data
+# Tell matplotlib you want interactive mode to plot live data
+plt.ion()
 
+# Printing serial configuration
 if arduinoData.is_open == True:
-    print(arduinoData, "\n")  # Printing serial configuration
+    print(arduinoData, "\n")
 
+fig, (ax1, ax2, ax3) = plt.subplots(3)
+# Create a function that configures our desired plot
 
-def makeFig():  # Create a function that makes our desired plot
-    plt.ylim(-10, 10)  # Set y min and max values
-    plt.title("My Live Streaming Sensor Data")  # Plot the title
-    plt.grid(True)  # Turn the grid on
-    plt.ylabel("Pressure [mmHg]")  # Set ylabels
-    plt.plot(Pressure, "ro-", label="mmHg")  # plot the pressure
-    plt.legend(loc="upper left")  # plot the legend
+def make_fig():
 
-    plt2 = plt.twinx()  # Create a second y axis
-    # plt2.ylim(-10, 10)  # Set limits of second y axis- adjust to readings you are getting
-    plt2.plot(Raw, "b^-", label="ELVH")  # plot pressure data
-    plt2.set_ylabel("ELVH")  # label second y axis
-    plt2.ticklabel_format(useOffset=False)  # Force matplotlib to NOT autoscale y axis
-    plt2.legend(loc="upper right")  # plot the legend
+    plt.subplot(311)
+    plt.plot(temperature, "r", label="C")
+    plt.ylabel("temperature")
+    plt.ticklabel_format(useOffset=False)
+    plt.tight_layout()
 
-    # saving to a csv file
-with open ('datalog_' + dt_string + '.csv', 'w') as file:
-    writer = csv.writer(file, delimiter=",")
-    writer.writerows(["elvh","lpdr"])
+    plt.subplot(312)
+    plt.plot(elvh_pressure, "b", label="mmHg")
+    plt.ylabel("ELVH pressure")
+    plt.ticklabel_format(useOffset=False)
+    plt.tight_layout()
 
-    while True:  # While loop that loops forever
-        while arduinoData.inWaiting() == 0:  # Wait here until there is data
-            pass  # do nothing
+    plt.subplot(313)
+    plt.plot(lpdr_pressure, "g", label="mmHg")
+    plt.ylabel("LPDR pressure")
+    plt.ticklabel_format(useOffset=False)
+    plt.tight_layout()
 
-        arduinoString = arduinoData.readline()  # read the line of text from the serial port
-        decoded = arduinoString.decode("utf-8")
-        dataArray = decoded.split(",")  # Split it into an array called dataArray
-        P = float(dataArray[0])  # Convert first element to float number and put in pressure
-        R = float(dataArray[1])  # Convert second element to float number and put in P
-        writer.writerow([dataArray[0], dataArray[1]]) # write to csv
-        Pressure.append(P)  # Build our Pressure array by appending pressure readings
-        Raw.append(R)  # Building our pressure array by appending P readings
-        drawnow(makeFig)  # Call drawnow to update our live graph
-        plt.pause(0.000001)  # Pause Briefly. Important to keep drawnow from crashing
-        cnt = cnt + 1
-        if cnt > 50:  # If you have 50 or more points, delete the first one from the array
-            Pressure.pop(0)  # This allows us to just see the last 50 data points
-            Raw.pop(0)
+def main():
+    # saving to a csv file and launching the plot GUI
+    with open('datalog_' + dt_string + '.csv', 'w',  encoding='UTF8', newline='') as file:
+        writer = csv.writer(file, delimiter=",")
+        writer.writerow(["time","elvh_temperature", "elvh_pressure", "lpdr_pressure"])
+
+        while True:  # While loop that loops forever
+            while arduinoData.inWaiting() == 0:  # Wait here until there is data
+                pass  # do nothing
+
+            # read the line of text from the serial port
+            arduinoString = arduinoData.readline()
+            # decode the string received on the serial port
+            decoded = arduinoString.strip().decode("utf-8")
+
+            # getting the time
+            now_update = datetime.now()
+            timer = now_update.strftime("%H:%M:%S")
+
+            print(timer +" "+ decoded)  # Printing the decoded data to the terminal (for debugging)
+
+            # Split it into an array called dataArray
+            dataArray = decoded.split(",")
+
+            ELVH_Temperature = float(dataArray[0])
+            ELVH_Pressure = float(dataArray[1])
+            LPDR_Pressure = float(dataArray[2])
+
+            if (ELVH_Pressure < 0):ELVH_Pressure = 0
+            if (LPDR_Pressure < 0):LPDR_Pressure = 0
+
+            # write to csv
+            writer.writerow([timer, ELVH_Temperature, ELVH_Pressure, LPDR_Pressure])
+            temperature.append(ELVH_Temperature)
+            elvh_pressure.append(ELVH_Pressure)
+            lpdr_pressure.append(LPDR_Pressure)
+
+            drawnow(make_fig)  # Call drawnow to update our live graph
+            # Pause Briefly. Important to keep drawnow from crashing
+            plt.pause(0.000001)
+            cnt = 0
+            cnt =+ 1
+            if cnt > 500:  # If you have 50 or more points, delete the first one from the array
+                # This allows us to just see the last 50 data points
+                temperature.pop(0)
+                elvh_pressure.pop(0)
+                lpdr_pressure.pop(0)
+
+if __name__ == "__main__":
+    main()
--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,42 @@
 #include <Wire.h>
 #include <AllSensors_DLV.h>
 
+#define IN_PIN A0
+#define WINDOW_SIZE 10
+
 AllSensors_DLV elvh_pressure(&Wire, AllSensors_DLV::SensorType::GAUGE, 50.0);
 float elvh_pressure_readings = 0.0;
+float elvh_temp_readings = 0.0;
+
+int INDEX = 0;
+float VALUE = 0;
+float SUM = 0;
+float READINGS[WINDOW_SIZE];
+float AVERAGED = 0;
+
+int INDEX_elvh_temperature = 0;
+float VALUE_elvh_temperature = 0;
+float SUM_elvh_temperature = 0;
+float READINGS_elvh_temperature[WINDOW_SIZE];
+float AVERAGED_elvh_temperature = 0;
+
+int INDEX_elvh_pressure = 0;
+float VALUE_elvh_pressure = 0;
+float SUM_elvh_pressure = 0;
+float READINGS_elvh_pressure[WINDOW_SIZE];
+float AVERAGED_elvh_pressure = 0;
 
 // pin usage
-const int pin = A0;
+const int pin = IN_PIN;
 
 void setup()
 {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(pin, INPUT);
+  // Setting the temperature and pressure units
+  elvh_pressure.setPressureUnit(AllSensors_DLV::PressureUnit::mmHg);
+  elvh_pressure.setTemperatureUnit(AllSensors_DLV::TemperatureUnit::CELSIUS);
 }
 
 void loop()
@@ -23,12 +48,41 @@ void loop()
   float pressure_in_inH2O = (raw_data - 0.25) / 0.375;  // in inH20 (water inch);
   float pressure_in_mmHg = pressure_in_inH2O * 1.86832; // in mmhg
 
+  // elvh sensor readings
   elvh_pressure.readData();
+  elvh_temp_readings = elvh_pressure.temperature;
   elvh_pressure_readings = elvh_pressure.pressure;
 
-  Serial.print(elvh_pressure_readings,2);
-  Serial.print(",");
-  Serial.println(pressure_in_mmHg, 2);
+  // Running average for LPDR Pressure sensor
+  SUM = SUM - READINGS[INDEX];       // Remove the oldest entry from the sum
+  VALUE = pressure_in_mmHg;          // Read the next sensor value
+  READINGS[INDEX] = VALUE;           // Add the newest reading to the window
+  SUM = SUM + VALUE;                 // Add the newest reading to the sum
+  INDEX = (INDEX + 1) % WINDOW_SIZE; // Increment the index, and wrap to 0 if it exceeds the window size
+  AVERAGED = SUM / WINDOW_SIZE;      // Divide the sum of the window by the window size for the result
 
-  delay(500);
+  // Running average for the elvh temperature sensor
+  SUM_elvh_temperature = SUM_elvh_temperature - READINGS_elvh_temperature[INDEX_elvh_temperature]; // Remove the oldest entry from the sum
+  VALUE_elvh_temperature = elvh_temp_readings;                                                     // Read the next sensor value
+  READINGS_elvh_temperature[INDEX_elvh_temperature] = VALUE_elvh_temperature;                      // Add the newest reading to the window
+  SUM_elvh_temperature = SUM_elvh_temperature + VALUE_elvh_temperature;                            // Add the newest reading to the sum
+  INDEX_elvh_temperature = (INDEX_elvh_temperature + 1) % WINDOW_SIZE;                             // Increment the index, and wrap to 0 if it exceeds the window size
+  AVERAGED_elvh_temperature = SUM_elvh_temperature / WINDOW_SIZE;                                  // Divide the sum of the window by the window size for the result*
+
+  // Running average for the elvh pressure sensor
+  SUM_elvh_pressure = SUM_elvh_pressure - READINGS_elvh_pressure[INDEX_elvh_pressure]; // Remove the oldest entry from the sum
+  VALUE_elvh_pressure = elvh_pressure_readings;                                        // Read the next sensor value
+  READINGS_elvh_pressure[INDEX_elvh_pressure] = VALUE_elvh_pressure;                   // Add the newest reading to the window
+  SUM_elvh_pressure = SUM_elvh_pressure + VALUE_elvh_pressure;                         // Add the newest reading to the sum
+  INDEX_elvh_pressure = (INDEX_elvh_pressure + 1) % WINDOW_SIZE;                       // Increment the index, and wrap to 0 if it exceeds the window size
+  AVERAGED_elvh_pressure = SUM_elvh_pressure / WINDOW_SIZE;                            // Divide the sum of the window by the window size for the result
+
+  // Serial printing
+  Serial.print(AVERAGED_elvh_temperature,1);
+  Serial.print(",");
+  Serial.print(AVERAGED_elvh_pressure,2);
+  Serial.print(",");
+  Serial.println(AVERAGED,2);
+
+  delay(1000);
 }
